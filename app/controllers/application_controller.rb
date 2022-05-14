2af160f48920bedb4296c81a8dff44b483f59682
kosa <a>
2022-05-14 12:12:26 +0900

    rubocop修正、HTMLのインデント修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception           #クロスサイトリクエストフォージェリ (CSRF)への対応策
+  protect_from_forgery with: :exception           # クロスサイトリクエストフォージェリ (CSRF)への対応策
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-                                                  #devise_controllerを使うときしか処理しない
+  # devise_controllerを使うときしか処理しない
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :comment])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :comment])
+  end
 end
