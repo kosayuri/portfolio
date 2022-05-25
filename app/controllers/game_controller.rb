4c54355debc979060af2fee1bbdfffdcc7ffbbfa
kosa <a>
2022-05-25 23:18:09 +0900

    ユーザー詳細ページで存在しないユーザーを指定された場合、フラッシュメッセージの表示と、ランキングページへの移動を実装

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -5,5 +5,10 @@ class GameController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+
+    if @user == nil
+      flash[:notice] = "指定のユーザーが存在しなかったため、ランキングページに戻りました。"
+      redirect_to(game_index_path)
+    end
   end
 end
