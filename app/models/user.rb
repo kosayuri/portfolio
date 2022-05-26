ce1b5f97a69d7698333029f30e9db83abc3f099d
kosa <a>
2022-05-27 01:33:35 +0900

    ゲームの設定用にUserテーブルにse_volume,bgm_volumeカラムの追加とバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 20 }
+  validates :se_volume, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+  validates :bgm_volume, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   mount_uploader :avatar, AvatarUploader
 end
