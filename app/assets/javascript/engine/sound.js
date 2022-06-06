0e1d61e9b05fb73a3b631fd507f35359ab9681ad
kosa <a>
2022-06-06 21:21:48 +0900

    ゲームプレイ時の音量変更機能の追加

--- app/assets/javascript/engine/sound.js
+++ app/assets/javascript/engine/sound.js
@@ -28,22 +28,27 @@ class NewSound {
 
 class Sound {
   constructor() {
+    let url = new URL(window.location.href);
+    let params = url.searchParams;
+    let se_volume = params.get('se_volume');
+    let bgm_volume = params.get('bgm_volume');
+
     this.load_progress = [0];
     this.bgm_stop_flag = false;
     this.bgm_context;
-    this.character_shot = new NewSound(this.load_progress, "/music/character_shot.ogg", 0.7);
-    this.enemy_shot = new NewSound(this.load_progress, "/music/enemy_shot.ogg", 0.4);
-    this.ricochet = new NewSound(this.load_progress, "/music/ricochet.ogg", 0.3);
-    this.enemy_die = new NewSound(this.load_progress, "/music/enemy_die.ogg", 0.7);
-    this.character_die = new NewSound(this.load_progress, "/music/character_die.ogg", 1);
-    this.item_get = new NewSound(this.load_progress, "/music/item_get.ogg", 0.4);
-    this.power_up = new NewSound(this.load_progress, "/music/power_up.ogg", 0.7);
-    this.boss_die = new NewSound(this.load_progress, "/music/boss_die.ogg", 0.9);
-    this.boss_advent = new NewSound(this.load_progress, "/music/boss_advent.ogg", 0.8);
-    this.clear = new NewSound(this.load_progress, "/music/clear.ogg", 1);
-    this.game_over = new NewSound(this.load_progress, "/music/game_over.ogg", 1);
-    this.stage_bgm = new NewSound(this.load_progress, "/music/stage_bgm.ogg", 0.5, 32.67104308);
-    this.boss_bgm = new NewSound(this.load_progress, "/music/boss_bgm.ogg", 0.8, 25.611);
+    this.character_shot = new NewSound(this.load_progress, "/music/character_shot.ogg", 0.7 * se_volume / 10);
+    this.enemy_shot = new NewSound(this.load_progress, "/music/enemy_shot.ogg", 0.4 * se_volume / 10);
+    this.ricochet = new NewSound(this.load_progress, "/music/ricochet.ogg", 0.3 * se_volume / 10);
+    this.enemy_die = new NewSound(this.load_progress, "/music/enemy_die.ogg", 0.7 * se_volume / 10);
+    this.character_die = new NewSound(this.load_progress, "/music/character_die.ogg", 1 * se_volume / 10);
+    this.item_get = new NewSound(this.load_progress, "/music/item_get.ogg", 0.4 * se_volume / 10);
+    this.power_up = new NewSound(this.load_progress, "/music/power_up.ogg", 0.7 * se_volume / 10);
+    this.boss_die = new NewSound(this.load_progress, "/music/boss_die.ogg", 0.9 * se_volume / 10);
+    this.boss_advent = new NewSound(this.load_progress, "/music/boss_advent.ogg", 0.8 * se_volume / 10);
+    this.clear = new NewSound(this.load_progress, "/music/clear.ogg", 1 * se_volume / 10);
+    this.game_over = new NewSound(this.load_progress, "/music/game_over.ogg", 1 * se_volume / 10);
+    this.stage_bgm = new NewSound(this.load_progress, "/music/stage_bgm.ogg", 0.5 * bgm_volume / 10, 32.67104308);
+    this.boss_bgm = new NewSound(this.load_progress, "/music/boss_bgm.ogg", 0.8 * bgm_volume / 10, 25.611);
   }
 
   main() {
