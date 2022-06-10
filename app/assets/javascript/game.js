590d97a3d7e936a16a7f7bbe5d1bc28e2a03f40c
kosa <a>
2022-06-11 00:47:27 +0900

    スマホでの操作に対応&ゲーム画面を最大化するよう修正

--- app/assets/javascript/game.js
+++ app/assets/javascript/game.js
@@ -38,8 +38,26 @@ addEventListener('load', () => {
   };
   let key_flag = true;
 
-  onkeydown = event => { if (key_flag) key_flag = false; if (event.code in key) key[event.code] = true; };
+  onkeydown = event => { if (event.code in key) key[event.code] = true; };
   onkeyup = event => { if (event.code in key) key[event.code] = false; };
+
+  document.getElementById("touch-character-menu").addEventListener('touchstart', () => {key["Escape"] = true;}, {passive: true});
+  document.getElementById("touch-character-menu").addEventListener('touchend', () => {key["Escape"] = false;});
+
+  document.getElementById("touch-character-left").addEventListener('touchstart', () => {key["ArrowLeft"] = true;}, {passive: true});
+  document.getElementById("touch-character-left").addEventListener('touchend', () => {key["ArrowLeft"] = false;});
+
+  document.getElementById("touch-character-up").addEventListener('touchstart', () => {key["ArrowUp"] = true;}, {passive: true});
+  document.getElementById("touch-character-up").addEventListener('touchend', () => {key["ArrowUp"] = false;});
+
+  document.getElementById("touch-character-right").addEventListener('touchstart', () => {key["ArrowRight"] = true;}, {passive: true});
+  document.getElementById("touch-character-right").addEventListener('touchend', () => {key["ArrowRight"] = false;});
+
+  document.getElementById("touch-character-down").addEventListener('touchstart', () => {key["ArrowDown"] = true;}, {passive: true});
+  document.getElementById("touch-character-down").addEventListener('touchend', () => {key["ArrowDown"] = false;});
+
+  document.getElementById("touch-character-shot").addEventListener('touchstart', () => {key["KeyZ"] = true;}, {passive: true});
+  document.getElementById("touch-character-shot").addEventListener('touchend', () => {key["KeyZ"] = false;});
 
   async function gamemain() {
     let ua = navigator.userAgent;
@@ -70,12 +88,13 @@ addEventListener('load', () => {
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, draw.canvas.width, draw.canvas.height);
     ctx.fillStyle = '#fff';
-    ctx.fillText("何かキーを押すとスタート", draw.canvas.width / 2, draw.canvas.height / 2);
+    ctx.fillText("操作ボタンかキーを押すとスタート", draw.canvas.width / 2, draw.canvas.height / 2);
     ctx.textAlign = "start";
     ctx.textBaseline = "alphabetic";
 
     while (key_flag) {
       await sleep(50);
+      Object.keys(key).forEach(variable_key =>{ if(key[variable_key]) key_flag=false;});
     }
 
     sound.stage_bgm.flag = 2;
