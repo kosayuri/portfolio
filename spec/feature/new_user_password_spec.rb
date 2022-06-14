735f68efd38ca49a166a87a919612519b42c1eaf
kosa <a>
2022-06-15 01:52:00 +0900

    beforeを追加して処理を共通化&ログイン状態とアクセス端末ごとでのテストの追加

--- spec/feature/new_user_password_spec.rb
+++ spec/feature/new_user_password_spec.rb
@@ -2,42 +2,39 @@ require 'rails_helper'
 
 RSpec.describe "devise/passwords/new.html.erb", type: :feature do
   describe "パスワードを忘れましたか？ページのテスト" do
-    it "ひま撃ちのリンクを正しく表示すること" do
+    before do
       visit new_user_password_path
+    end
+
+    it "ひま撃ちのリンクを正しく表示すること" do
       expect(page).to have_link "ひま撃ち", href: root_path
     end
 
     it "アカウント登録のリンクを正しく表示すること" do
-      visit new_user_password_path
       expect(page).to have_link "アカウント登録", href: new_user_registration_path
     end
 
     it "ヘッダーのログインのリンクを正しく表示すること" do
-      visit new_user_password_path
       within(:css, 'div[id="header"]') do
         expect(page).to have_link "ログイン", href: new_user_session_path
       end
     end
 
     it "フォームのリンクが正しいこと" do
-      visit new_user_password_path
       expect(page).to have_selector "form[action='#{user_password_path}']"
     end
 
     it "Eメールの入力欄を表示すること" do
-      visit new_user_password_path
       expect(page).to have_selector 'input[name="user[email]"]'
     end
 
     it "送信ボタンの下にあるログインのリンクを正しく表示すること" do
-      visit new_user_password_path
       within(:css, 'div[class="account-forum"]') do
         expect(page).to have_link "ログイン", href: new_user_session_path
       end
     end
 
     it "送信ボタンの下にあるアカウント登録のリンクを正しく表示すること" do
-      visit new_user_password_path
       within(:css, 'div[class="account-forum"]') do
         expect(page).to have_link "アカウント登録", href: new_user_registration_path
       end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,9 +2,36 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe "トップページのコントローラテスト" do
-    it "200レスポンスすること" do
-      get root_url
-      expect(response.status).to eq 200
+    let(:user) { create(:user) }
+    let(:game_status) {{ hiscore: user.hiscore, se_volume: user.se_volume, bgm_volume: user.bgm_volume }}
+
+    context "ログインしている場合" do
+      before do
+        sign_in user
+        get root_url
+      end
+
+      it "200レスポンスすること" do
+        expect(response.status).to eq 200
+      end
+
+      it "@game_statusに値を代入すること" do
+        expect(controller.instance_variable_get(:@game_status)).to eq(game_status)
+      end
+    end
+
+    context "ログインしていない場合" do
+      before do
+        get root_url
+      end
+
+      it "200レスポンスすること" do
+        expect(response.status).to eq 200
+      end
+
+      it "@game_statusに値を代入しないこと" do
+        expect(controller.instance_variable_get(:@game_status)).to eq(nil)
+      end
     end
   end
 end
