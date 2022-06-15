708d318072d6e9a7130f22785d58e0304fde6af5
kosa <a>
2022-06-15 22:28:36 +0900

    userモデルにゲストユーザーを作成するメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   validates :se_volume, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :bgm_volume, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   mount_uploader :avatar, AvatarUploader
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com', name: 'ゲスト') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
