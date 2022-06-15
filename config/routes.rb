04bb5dbef248b627794ef33c838f209010c087e5
kosa <a>
2022-06-16 00:54:50 +0900

    ゲストユーザーを削除防止、メールアドレスとパスワードの変更防止きのうの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :game, only: [:index, :show, :edit, :update, :new, :create]
   root 'home#index'
-  devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
