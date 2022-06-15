0f318480780da320565fe2d6f5b366fd04f611b3
kosa <a>
2022-06-15 22:29:12 +0900

    ゲストログイン用のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :game, only: [:index, :show, :edit, :update, :new, :create]
   root 'home#index'
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
