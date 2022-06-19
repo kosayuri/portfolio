a9c7325b7f6606a2657edb10b2ca250ceb8d9852
kosa <a@a.a>
2022-06-20 00:11:04 +0900

    dbのカラムcommentの初期値の指定を削除

--- db/migrate/20220508035708_devise_create_users.rb
+++ db/migrate/20220508035708_devise_create_users.rb
@@ -33,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
 
       t.string :name, null: false, default: ""
-      t.text :comment, null: true, default: ""
+      t.text :comment, null: true
       t.bigint :hiscore, null: false, default: "0"
       t.timestamps null: false
     end
